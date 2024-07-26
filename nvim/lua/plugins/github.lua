9189190c8c710053ce1720d3b5e44f8ca6438fdc
capoyon <email.cristopher@gmail.com>
2024-07-26 10:53:44 +0800

    git keybindings

--- nvim/lua/plugins/github.lua
+++ nvim/lua/plugins/github.lua
@@ -1,11 +1,47 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require("gitsigns").setup()
+        require('gitsigns').setup{
+            on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
 
-        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-        vim.keymap.set("n", "<leader>glb", ":Gitsigns toggle_current_line_blame<CR>", {})
-        vim.keymap.set("n", "<leader>ga", ":Gitsigns stage_hunk<CR>", {})
-        vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", {})
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', ']g', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({']c', bang = true})
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+                map('n', '[g', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({'[c', bang = true})
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end)
+
+                map('n', '<leader>ga', gitsigns.stage_hunk)
+                map('n', '<leader>gr', gitsigns.reset_hunk)
+                map('v', '<leader>gs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                map('v', '<leader>gr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                map('n', '<leader>gS', gitsigns.stage_buffer)
+                map('n', '<leader>gu', gitsigns.undo_stage_hunk)
+                map('n', '<leader>gR', gitsigns.reset_buffer)
+                map('n', '<leader>gp', gitsigns.preview_hunk)
+                map('n', '<leader>gb', function() gitsigns.blame_line{full=true} end)
+                map('n', '<leader>glb', gitsigns.toggle_current_line_blame)
+                map('n', '<leader>gd', gitsigns.diffthis)
+                map('n', '<leader>gD', function() gitsigns.diffthis('~') end)
+                map('n', '<leader>gd', gitsigns.toggle_deleted)
+            end
+        }
     end
 }
