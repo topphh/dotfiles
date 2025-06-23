cea707e62d02afe18b2840106d65b0c33a40ea7e
topphh <email.cristopher@gmail.com>
2025-06-23 16:40:18 +0800

    update go formatter

--- nvim/lua/configs/null-ls.lua
+++ nvim/lua/configs/null-ls.lua
@@ -3,9 +3,9 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 return {
   sources = {
-    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports_reviser,
-    null_ls.builtins.formatting.golines,
+    --null_ls.builtins.formatting.golines,
   },
 
   -- format on save
