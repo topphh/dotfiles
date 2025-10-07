d7ac53b5275144545f856dd7a4e78dc24555bf0f
topphh <email.cristopher@gmail.com>
2025-10-07 11:42:14 +0800

    update co pilot config

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,40 +1,26 @@
 return {
-  -- CopilotChat with custom configuration
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     event = "VeryLazy",
     dependencies = {
       { "nvim-lua/plenary.nvim", branch = "master" },
-      { "github/copilot.vim" }, -- Make sure we have the base copilot
+      { "github/copilot.vim" },
     },
     build = "make tiktoken",
-    opts = function()
-      return require("configs.copilot").setup()
-    end,
-    config = function(_, opts)
-      local chat = require("CopilotChat")
-      chat.setup(opts)
-      
-      -- Custom keymaps for CopilotChat (all using z prefix)
-      vim.keymap.set("n", "zc", ":CopilotChat ", { desc = "CopilotChat - Open chat" })
-      vim.keymap.set("n", "zt", ":CopilotChatToggle<CR>", { desc = "CopilotChat - Toggle chat" })
-      vim.keymap.set("n", "zf", ":CopilotChatFix<CR>", { desc = "CopilotChat - Fix code" })
-      vim.keymap.set("n", "zo", ":CopilotChatOptimize<CR>", { desc = "CopilotChat - Optimize code" })
-      vim.keymap.set("n", "zd", ":CopilotChatDocs<CR>", { desc = "CopilotChat - Add documentation" })
-      vim.keymap.set("n", "zp", ":CopilotChatTests<CR>", { desc = "CopilotChat - Generate tests" })
-      vim.keymap.set("n", "zr", ":CopilotChatReset<CR>", { desc = "CopilotChat - Reset chat" })
-      
-      -- Explain code keymaps using z prefix
-      vim.keymap.set("n", "ze", function()
-        chat.ask("Explain this code", { selection = require("CopilotChat.select").buffer })
-      end, { desc = "CopilotChat - Explain current buffer" })
-      
-      vim.keymap.set("v", "ze", function()
-        chat.ask("Explain this code", { selection = require("CopilotChat.select").visual })
-      end, { desc = "CopilotChat - Explain selection" })
-    end,
+    opts = {},
+    keys = {
+      { "<leader>zc", ":CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+      { "<leader>ze", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain code" },
+      { "<leader>zr", ":CopilotChatReview<CR>", mode = "v", desc = "Review code" },
+      { "<leader>zf", ":CopilotChatFix<CR>", mode = "v", desc = "Fix code issues" },
+      { "<leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize code" },
+      { "<leader>zd", ":CopilotChatDocs<CR>", mode = "v", desc = "Generate docs" },
+      { "<leader>zt", ":CopilotChatTests<CR>", mode = "v", desc = "Generate tests" },
+      { "<leader>zm", ":CopilotChatCommit<CR>", mode = "n", desc = "Generate commit message" },
+      { "<leader>zm", ":CopilotChatCommit<CR>", mode = "v", desc = "Generate commit message for selection" },
+    },
   },
-  
+
   -- Base GitHub Copilot for inline suggestions
   {
     "github/copilot.vim",
@@ -42,25 +28,25 @@ return {
     config = function()
       -- Disable default tab mapping since NvChad uses it
       vim.g.copilot_no_tab_map = true
-      
+
       -- Set custom accept mapping to <C-y> in insert mode (avoids <C-z> suspend conflict)
       vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
-        desc = "Accept Copilot suggestion"
+        desc = "Accept Copilot suggestion",
       })
-      
+
       -- Other Copilot keymaps for navigating suggestions
       vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)", { desc = "Next Copilot suggestion" })
       vim.keymap.set("i", "<C-[>", "<Plug>(copilot-previous)", { desc = "Previous Copilot suggestion" })
       vim.keymap.set("i", "<C-\\>", "<Plug>(copilot-dismiss)", { desc = "Dismiss Copilot suggestion" })
-      
+
       -- Additional robust escape handling for Copilot
       vim.keymap.set("i", "<C-c>", function()
-        if vim.fn.exists('*copilot#GetDisplayedSuggestion') == 1 then
-          local suggestion = vim.fn['copilot#GetDisplayedSuggestion']()
-          if suggestion.text ~= '' then
-            vim.fn['copilot#Dismiss']()
+        if vim.fn.exists "*copilot#GetDisplayedSuggestion" == 1 then
+          local suggestion = vim.fn["copilot#GetDisplayedSuggestion"]()
+          if suggestion.text ~= "" then
+            vim.fn["copilot#Dismiss"]()
           end
         end
         return "<C-c>"
